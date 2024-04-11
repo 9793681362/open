3eff92e2ce7c64510c4a65e5e0b77c901b358877
9793681362 <135099268+9793681362@users.noreply.github.com>
2024-04-11 17:10:34 +0800

    提交

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 let spu = document.getElementById('spu')
 
 spu.addEventListener("click", function () {
-    window.location.href = '../product.html'
+    window.location.href = 'http://rick1001b.cn/open/product.html'
 })
